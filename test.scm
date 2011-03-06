d27fbb4ea5649652562b3cfcb451048cbf8a91fd
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2011-03-06 16:11:15 -0500

    Fixed CS.

--- test.scm
+++ test.scm
@@ -3,6 +3,7 @@
 exec csi -ss $0 ${1+"$@"}
 exit
 |#
-(load "scriptedmain.scm")
-(display (format "Test: The meaning of life is ~a\n" (meaning-of-life)))
-(exit)
+(define (main args)
+	(load "scriptedmain.scm")
+	(display (format "Test: The meaning of life is ~a\n" (meaning-of-life)))
+	(exit))
