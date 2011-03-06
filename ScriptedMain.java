0bb045e6ea8435e9ac8f54a70ce2479c1764ddef
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2011-03-06 15:07:29 -0500

    Improved Java.

--- ScriptedMain.java
+++ ScriptedMain.java
@@ -1,13 +1,9 @@
 public class ScriptedMain {
+	public static int meaningOfLife() {
+		return 42;
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Directory: " + ScriptedMain.class.getProtectionDomain().getCodeSource().getLocation().getPath());
-
-		System.out.println("Program: " + ScriptedMain.class.getName());
-
-		System.out.println("Number of Args: " + args.length);
-
-		for (int i = 0; i < args.length; i++) {
-			System.out.println("Arg: " + args[i]);
-		}
+		System.out.println("Main: The meaning of life is " + meaningOfLife());
 	}
 }
--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 public class Test {
 	public static void main(String[] args) {
-		ScriptedMain sm = new ScriptedMain();
+		System.out.println("Test: The meaning of life is " + ScriptedMain.meaningOfLife());
 	}
 }
