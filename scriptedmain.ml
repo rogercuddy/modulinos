f398d36ca522d67739b1cdf83ca656cd30297538
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2011-09-30 16:48:57 -0400

    simplified

--- scriptedmain.ml
+++ scriptedmain.ml
@@ -12,11 +12,11 @@ Run:
 
 let meaning_of_life = 42
 
-let rec main () = print_endline ("Main: The meaning of life is " ^ string_of_int meaning_of_life)
+let main () = print_endline ("Main: The meaning of life is " ^ string_of_int meaning_of_life)
 
 let _ =
-	let program = Sys.argv.(0) in
-		let re = Str.regexp "scriptedmain" in
+	let program = Sys.argv.(0)
+	and re = Str.regexp "scriptedmain" in
 		try let _ = Str.search_forward re program 0 in
 			main ()
 		with _ -> ()
--- test.ml
+++ test.ml
@@ -10,11 +10,11 @@ Run:
 
 *)
 
-let rec main () = print_endline ("Test: The meaning of life is " ^ string_of_int Scriptedmain.meaning_of_life)
+let main () = print_endline ("Test: The meaning of life is " ^ string_of_int Scriptedmain.meaning_of_life)
 
 let _ =
-	let program = Sys.argv.(0) in
-		let re = Str.regexp "test" in
+	let program = Sys.argv.(0)
+	and re = Str.regexp "test" in
 		try let _ = Str.search_forward re program 0 in
 			main ()
 		with _ -> ()
