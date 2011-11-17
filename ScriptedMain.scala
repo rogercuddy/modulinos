17bc15d75e02ac42c58507cd30ece786b0fdf65e
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2011-11-17 11:38:32 -0500

    debugging Scala

---
+++ ScriptedMain.scala
@@ -0,0 +1,7 @@
+object ScriptedMain {
+	val meaningOfLife = 42
+
+	def main(args: Array[String]) {
+		println("Main: The meaning of life is " + meaningOfLife)
+	}
+}
---
+++ Test.scala
@@ -0,0 +1,5 @@
+object Test {
+	def main(args: Array[String]) {
+		println("Test: The meaning of life is " + (new ScriptedMain).meaningOfLife)
+	}
+}
