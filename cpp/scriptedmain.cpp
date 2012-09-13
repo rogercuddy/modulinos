2d7766f32b61dd8a6de6d491f89ba086a1431ead
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-09-12 20:26:54 -0400

    C++ working in Windows as well as Unix

--- cpp/scriptedmain.cpp
+++ cpp/scriptedmain.cpp
@@ -6,8 +6,12 @@ int meaning_of_life() {
 	return 42;
 }
 
-int __attribute__((weak)) main(int argc, char **argv) {
+#ifdef SCRIPTEDMAIN
+
+int main() {
 	cout << "Main: The meaning of life is " << meaning_of_life() << endl;
 
 	return 0;
 }
+
+#endif
