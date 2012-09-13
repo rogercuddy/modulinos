abcc5d031c0c69553504f375331cfae76f4df63b
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-09-12 20:16:19 -0400

    C working in Unix and Windows

--- c/scriptedmain.c
+++ c/scriptedmain.c
@@ -4,8 +4,12 @@ int meaning_of_life() {
 	return 42;
 }
 
-int __attribute__((weak)) main() {
+#ifdef SCRIPTEDMAIN
+
+int main() {
 	printf("Main: The meaning of life is %d\n", meaning_of_life());
 
 	return 0;
 }
+
+#endif
