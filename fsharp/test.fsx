41e381a419c0edab2583166abc8ee2f9da4fd8cc
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-01-18 21:03:29 -0500

    resolving errors

--- fsharp/test.fsx
+++ fsharp/test.fsx
@@ -2,7 +2,22 @@
 	exec fsharpi --exec $0 --quiet
 *)
 
+let scriptname =
+    let args = System.Environment.GetCommandLineArgs()
+
+    let arg0 = args.[0]
+
+    if arg0.Contains("fsi") then
+        let arg1 = args.[1]
+        if arg1 = "--exec" then
+            args.[2]
+        else
+            arg1
+    else
+        arg0
+
 #load "scriptedmain.fsx"
 
 let main =
-    printfn "Test: The meaning of life is %d" ScriptedMain.meaningOfLife
+    if scriptname.Contains("test") then
+        printfn "Test: The meaning of life is %d" ScriptedMain.meaningOfLife
