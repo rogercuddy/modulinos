bed48d6a3f9f737a193a2e0690b9e2e9f75901c7
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-02 12:09:10 -0400

    tabs

--- ruby/scriptedmain.rb
+++ ruby/scriptedmain.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
 
 def meaning_of_life
-	42
+  42
 end
 
 def main
-	puts "Main: The meaning of life is #{meaning_of_life}"
+  puts "Main: The meaning of life is #{meaning_of_life}"
 end
 
 if __FILE__ == $0
-	main
+  main
 end
