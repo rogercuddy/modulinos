bdd49d3272ffca892d93dade4f8c3e32d387e8a1
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-02 16:03:43 -0400

    perl: strict, warnings, windows and unix

--- perl/ScriptedMain.pm
+++ perl/ScriptedMain.pm
@@ -1,14 +1,14 @@
-#!/usr/bin/env perl
+#!/usr/bin/env perl -sw
 
 use strict;
 use warnings;
 
 sub meaning_of_life {
-	return 42;
+  return 42;
 }
 
 sub main {
-	print "Main: The meaning of life is " . meaning_of_life . "\n";
+  print "Main: The meaning of life is " . meaning_of_life . "\n";
 }
 
 main unless caller;
--- perl/test.pl
+++ perl/test.pl
@@ -1,6 +1,8 @@
-#!/usr/bin/env perl
+#!/usr/bin/env perl -sw
 
 use strict;
+use warnings;
+
 use ScriptedMain;
 
 print "Test: The meaning of life is " . meaning_of_life . "\n";
