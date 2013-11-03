bd1eb1eb798e902d21ea24d371fbd7a8a27ea7d0
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-02 23:53:47 -0400

    spacing

--- java/Test.java
+++ java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
-	public static void main(String[] args) {
-		System.out.println("Test: The meaning of life is " + ScriptedMain.meaningOfLife());
-	}
+  public static void main(String[] args) {
+    System.out.println("Test: The meaning of life is " + ScriptedMain.meaningOfLife());
+  }
 }
