b10025f7dc4482107ed4150d98bbfef825d97146
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-03 00:58:20 -0400

    extra ocaml warnings

--- ocaml/scriptedmain.ml
+++ ocaml/scriptedmain.ml
@@ -1,5 +1,5 @@
-if true then ignore begin let kkkk _ _ _ _ _ = 0 in kkkk
-"exec" "ocaml" "str.cma" "$0" "$@" + let fi = 0 and exit _ _ = 0 in if false
+if true then ignore begin let kkkk _ _ _ _ _ _ _ = 0 in kkkk
+"exec" "ocaml" "-w" "+a" "str.cma" "$0" "$@" + let fi = 0 and exit _ _ = 0 in if false
 then exit
 fi
 true else 0
@@ -13,7 +13,7 @@ Interpret:
 
 Compile:
 
-ocamlc -o scriptedmain -linkall str.cma scriptedmain.ml
+ocamlc -w +a -o scriptedmain -linkall str.cma scriptedmain.ml
 
 Run:
 
@@ -26,8 +26,8 @@ let meaning_of_life : int = 42
 let main () = print_endline ("Main: The meaning of life is " ^ string_of_int meaning_of_life)
 
 let _ =
-	let program = Sys.argv.(0)
-	and re = Str.regexp "scriptedmain" in
-		try let _ = Str.search_forward re program 0 in
-			main ()
-		with Not_found -> ()
+  let program = Sys.argv.(0)
+  and re = Str.regexp "scriptedmain" in
+    try let _ = Str.search_forward re program 0 in
+      main ()
+    with Not_found -> ()
--- ocaml/test.ml
+++ ocaml/test.ml
@@ -1,5 +1,5 @@
-if true then ignore begin let kkkk _ _ _ _ _ _ = 0 in kkkk
-"exec" "ocaml" "str.cma" "scriptedmain.cmo" "$0" "$@" + let fi = 0 and exit _ _ = 0 in if false
+if true then ignore begin let kkkk _ _ _ _ _ _ _ _ = 0 in kkkk
+"exec" "ocaml" "-w" "+a" "str.cma" "scriptedmain.cmo" "$0" "$@" + let fi = 0 and exit _ _ = 0 in if false
 then exit
 fi
 true else 0
@@ -7,13 +7,13 @@ end;;
 
 (*
 
-Compile:
-
-ocamlc -o test -linkall str.cma scriptedmain.ml test.ml
-
 Interpret:
 
 ./test.ml
+
+Compile:
+
+ocamlc -w +a -o test -linkall str.cma scriptedmain.ml test.ml
 
 Run:
 
@@ -24,8 +24,8 @@ Run:
 let main () = print_endline ("Test: The meaning of life is " ^ string_of_int Scriptedmain.meaning_of_life)
 
 let _ =
-	let program = Sys.argv.(0)
-	and re = Str.regexp "test" in
-		try let _ = Str.search_forward re program 0 in
-			main ()
-		with Not_found -> ()
+  let program = Sys.argv.(0)
+  and re = Str.regexp "test" in
+    try let _ = Str.search_forward re program 0 in
+      main ()
+    with Not_found -> ()
