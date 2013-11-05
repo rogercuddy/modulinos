eec53fe0cfa19cf51915c010512b2cacb65136d2
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-04 22:20:22 -0500

    clang++ -Wmost -Weverything

--- cpp/scriptedmain.cpp
+++ cpp/scriptedmain.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+#include "scriptedmain.h"
 
 using namespace std;
 
 int meaning_of_life() {
-	return 42;
+  return 42;
 }
 
 #ifdef SCRIPTEDMAIN
 
 int main() {
-	cout << "Main: The meaning of life is " << meaning_of_life() << endl;
-	return 0;
+  cout << "Main: The meaning of life is " << meaning_of_life() << endl;
+  return 0;
 }
 
 #endif
--- cpp/test.cpp
+++ cpp/test.cpp
@@ -6,6 +6,6 @@ using namespace std;
 extern int meaning_of_life();
 
 int main() {
-	cout << "Test: The meaning of life is " << meaning_of_life() << endl;
-	return 0;
+  cout << "Test: The meaning of life is " << meaning_of_life() << endl;
+  return 0;
 }
