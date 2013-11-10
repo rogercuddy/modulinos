537ea6c58ed657d34899bdf225673420f85a557d
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-09 23:45:52 -0500

    pyflakes

--- python/tasks.py
+++ python/tasks.py
@@ -1,5 +1,18 @@
 from invoke import run, task
 
 @task
-def lint():
+def test():
+  run("python scriptedmain.py")
+  run("python test.py")
+
+@task
+def pylint():
   run("pylint *.py")
+
+@task
+def pyflakes():
+  run("pyflakes .")
+
+@task("pylint", "pyflakes")
+def lint():
+  pass
