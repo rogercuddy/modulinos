082da2349e4f1ffe206f70a9fd731a381ba71e61
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-12-12 18:55:36 -0500

    checkstyle

--- java/ScriptedMain.java
+++ java/ScriptedMain.java
@@ -1,10 +1,20 @@
+/** Main script */
 public final class ScriptedMain {
+  /** utility class */
   private ScriptedMain() {}
 
+  /**
+     <p>H2G2 meaning of life</p>
+     @return meaning of life
+  */
   public static int meaningOfLife() {
     return 42;
   }
 
+  /**
+     <p>Main app</p>
+     @param args CLI arguments
+  */
   public static void main(final String[] args) {
     System.out.println("Main: The meaning of life is " + meaningOfLife());
   }
--- java/Test.java
+++ java/Test.java
@@ -1,6 +1,12 @@
+/** Test script */
 public final class Test {
+  /** utility class */
   private Test() {}
 
+  /**
+     <p>Test app</p>
+     @param args CLI arguments
+  */
   public static void main(final String[] args) {
     System.out.println("Test: The meaning of life is " + ScriptedMain.meaningOfLife());
   }
