b42b8a6281b855931afc2c99765614d27057fb2d
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-01-13 21:42:00 -0500

    final

--- groovy/ScriptedMain.groovy
+++ groovy/ScriptedMain.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 class ScriptedMain {
-	static def meaningOfLife = 42
+	static final def meaningOfLife = 42
 
 	static main(final args) {
 		println "Main: The meaning of life is " + meaningOfLife
