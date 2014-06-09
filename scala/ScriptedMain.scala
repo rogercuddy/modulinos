7fb84aa48e5c2dd9c91a96d5e30ac98a53ae0a72
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-06-09 10:14:09 -0500

    scala idioms

--- scala/ScriptedMain.scala
+++ scala/ScriptedMain.scala
@@ -1,10 +1,14 @@
 // Run:
+//
 // scala ScriptedMain.scala
+//
+// Compile/Run:
+//
+// scalac ScriptedMain.scala
+// scala ScriptedMain
 
 object ScriptedMain {
   val meaningOfLife = 42
 
-  def main(args: Array[String]) {
-    println("Main: The meaning of life is " + meaningOfLife)
-  }
+  def main(args: Array[String]) : Unit = println("Main: The meaning of life is " + meaningOfLife)
 }
--- scala/Test.scala
+++ scala/Test.scala
@@ -1,12 +1,11 @@
 // Compile:
-// scalac ScriptedMain.scala
-// scalac -classpath . Test.scala
 //
-// Run (either CLASSPATH must contain the current directory (.), or the CLASSPATH must be unset):
+// scalac ScriptedMain.scala Test.scala
+//
+// Run:
+//
 // scala Test
 
 object Test {
-  def main(args: Array[String]) {
-    println("Test: The meaning of life is " + ScriptedMain.meaningOfLife)
-  }
+  def main(args: Array[String]) : Unit = println("Test: The meaning of life is " + ScriptedMain.meaningOfLife)
 }
