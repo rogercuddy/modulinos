7f0960f0c79037ed0db6fe1fa5ac9df37cf2e358
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-07-27 19:48:37 -0500

    main

--- chicken/scriptedmain.scm
+++ chicken/scriptedmain.scm
@@ -1,21 +1,20 @@
 #!/usr/bin/env csi -ss
 
-(use posix)
+(require-extension srfi-13) ; strings
 (require-extension srfi-1) ; lists
 
-(define (meaning-of-life) 42)
-
 (define (main args)
-	(display (format "Main: The meaning of life is ~a\n" (meaning-of-life)))
-	(exit))
+  (display (format "Main: The meaning of life is ~a\n" (meaning-of-life))))
 
 (define (program)
-	(if (string=? (car (argv)) "csi")
-		(let ((s-index (list-index (lambda (x) (string-contains x "-s")) (argv))))
-			(if (number? s-index)
-				(cons 'interpreted (list-ref (argv) (+ 1 s-index)))
-				(cons 'unknown "")))
-		(cons 'compiled (car (argv)))))
+  (if (string=? (car (argv)) "csi")
+      (let ((s-index (list-index (lambda (x) (string-contains x "-s")) (argv))))
+        (if (number? s-index)
+            (cons 'interpreted (list-ref (argv) (+ 1 s-index)))
+            (cons 'unknown "")))
+      (cons 'compiled (car (argv)))))
 
 (if (equal? (car (program)) 'compiled)
-	(main (cdr (argv))))
+    (main (cdr (argv))))
+
+(define (meaning-of-life) 42)
--- chicken/test.scm
+++ chicken/test.scm
@@ -1,6 +1,19 @@
 #!/usr/bin/env csi -ss
 
+(require-extension srfi-13) ; strings
+(require-extension srfi-1) ; lists
+
 (define (main args)
-	(load "scriptedmain.scm")
-	(display (format "Test: The meaning of life is ~a\n" (meaning-of-life)))
-	(exit))
+  (load "scriptedmain.scm")
+  (display (format "Test: The meaning of life is ~a\n" (meaning-of-life))))
+
+(define (program)
+  (if (string=? (car (argv)) "csi")
+      (let ((s-index (list-index (lambda (x) (string-contains x "-s")) (argv))))
+        (if (number? s-index)
+            (cons 'interpreted (list-ref (argv) (+ 1 s-index)))
+            (cons 'unknown "")))
+      (cons 'compiled (car (argv)))))
+
+(if (equal? (car (program)) 'compiled)
+    (main (cdr (argv))))
