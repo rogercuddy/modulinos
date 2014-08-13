db9588f69f32d4759401ea2c859ce203f48a3d79
Andy Pennebaker <andy.pennebaker@wicourts.gov>
2014-08-13 08:53:00 -0500

    removed unnecessary code

--- chicken/test.scm
+++ chicken/test.scm
@@ -1,22 +1,8 @@
 ":"; exec csi -ss $0 ${1+"$@"}
 
-(declare (uses chicken-syntax))
-
-(use srfi-1) ; lists
-(use srfi-13) ; strings
-(use regex)
-
 (define (main args)
 	(load "scriptedmain")
   (display (format "Test: The meaning of life is ~a\n" (meaning-of-life))))
-
-(define (program)
-  (if (string=? (car (argv)) "csi")
-      (let ((s-index (list-index (lambda (x) (string-contains x "-s")) (argv))))
-        (if (number? s-index)
-            (cons 'interpreted (list-ref (argv) (+ 1 s-index)))
-            (cons 'unknown "")))
-      (cons 'compiled (car (argv)))))
 
 (cond-expand
  (chicken-compile-shared)
