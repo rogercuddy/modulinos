1e5628c85757d92099d2e157e103eb84f92867d6
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-12-02 10:56:36 -0600

    debugging module errors

--- swift/ScriptedMain.swift
+++ swift/ScriptedMain.swift
@@ -2,14 +2,18 @@ import Foundation
 
 public class ScriptedMain {
   public var meaningOfLife = 42
+
+  public class func main() {
+    var meaning = ScriptedMain().meaningOfLife
+
+    println("Main: The meaning of life is \(meaning)")
+  }
 }
 
 #if SCRIPTEDMAIN
 @objc class ScriptedMainAutoload {
   @objc class func load() {
-    var meaning = ScriptedMain().meaningOfLife
-
-    println("Main: The meaning of life is \(meaning)")
+    ScriptedMain.main()
   }
 }
 #endif
--- swift/Test.swift
+++ swift/Test.swift
@@ -1,11 +1,17 @@
 import Foundation
 
-#if TEST
-@objc class TestAutoload {
-  @objc class func load() {
+public class Test {
+  public class func main() {
     var meaning = ScriptedMain().meaningOfLife
 
     println("Test: The meaning of life is \(meaning)")
+  }
+}
+
+#if TEST
+@objc class TestAutoload {
+  @objc class func load() {
+    Test.main()
   }
 }
 #endif
