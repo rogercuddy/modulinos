dfdbe7fd11b4204be7fc0feb8969c6c5f8e82fb6
Andrew Pennebaker <andrew.pennebaker@shoutlet.com>
2015-12-10 15:15:08 -0600

    string interpolation

--- scala/src/main/scala/us/yellosoft/Modulino.scala
+++ scala/src/main/scala/us/yellosoft/Modulino.scala
@@ -3,5 +3,5 @@ package us.yellosoft.modulino
 object Modulino {
   val meaningOfLife = 42
 
-  def main(args: Array[String]) : Unit = println("Main: The meaning of life is " + meaningOfLife)
+  def main(args: Array[String]) : Unit = println(s"Main: The meaning of life is $meaningOfLife")
 }
